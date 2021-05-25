286e93ebf1f52151d4eeac515b187d0c2d8ad640
alexandrer0x <alexandrer0x@hotmail.com>
2021-05-25 16:23:56 -0300

    README and LICENSE

--- README.md
+++ README.md
@@ -3,5 +3,15 @@
 ### About
 This project is an example of a REST API for a delivery application, developed only for practice purposes.
 
+### Technologies
+Java 11  
+Maven  
+Spring: Boot, Web, Data JPA, Validation  
+Hibernate  
+MySQL  
+Flyway  
+Lombok  
+Model Mapper  
+
 ### Contact
 alexandrevs@outlook.com.
--- src/main/java/dev/alexandrevieira/delivery/DeliveryApplication.java
+++ src/main/java/dev/alexandrevieira/delivery/DeliveryApplication.java
@@ -1,18 +1,12 @@
 package dev.alexandrevieira.delivery;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeliveryApplication implements CommandLineRunner {
+public class DeliveryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeliveryApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
 	}
 }
